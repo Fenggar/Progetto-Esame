f05a99a4ed51ab1b6c9f71ecc3f4e9de32500a52
Davide <Davide@CompiutoMK3.lan>
2021-01-06 12:13:44 +0100

    Scritto JAVADoc su services, gestioneFile e RestController.

--- MeteoRite/src/main/java/ProgettoEsame/MeteoRite/Model/Services.java
+++ MeteoRite/src/main/java/ProgettoEsame/MeteoRite/Model/Services.java
@@ -19,137 +19,21 @@ public class Services
 	private JSONArray ja = null;
 	private APICall call = new APICall("Pandoiano");
 	
-	public void nameToID(String nome) throws MalformedURLException
-	{
-		String ID ="";
-		
-		forecastNome(nome);
-		System.out.println("CHIAMATO FORCASTNAME DA nameToID");
-		JSONObject app = call.getCast();
-		JSONObject city = (JSONObject) app.get("city");
-		Object idCitta =city.get("id");
-		System.out.println("idCitta: "+idCitta);
-		
-		ID =idCitta.toString();
-		System.out.println("id:" +ID);
-		call.setId(ID);
-		
-		System.out.println("CONVERTITO ID");
-		
-	}
-	
-	public JSONObject call() throws MalformedURLException
-	{
-		APICall prova = new APICall("Pontedera","&units=metric");
-		String indirizzo = "http://api.openweathermap.org/data/2.5/forecast?q=" +prova.getNomeCitta() +"&appid=" +prova.getKey() +prova.getUnita();
-		
-		URL url = new URL (indirizzo);
-		
-		try {
-			URLConnection openConnection = new URL(indirizzo).openConnection();
-			InputStream in = openConnection.getInputStream();
-			
-			String data = "";
-			String line = "";
-			try {
-			   InputStreamReader inR = new InputStreamReader( in );
-			   BufferedReader buf = new BufferedReader( inR );
-			  
-			   while ( ( line = buf.readLine() ) != null ) {
-				   data+= line;
-			   }
-			} 
-			finally 
-			{
-			   in.close();
-			}
-			
-				this.jo = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
-				//System.out.println("JSONObject scaricato: "+ jo);
-			/*} else {
-				ja = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
-				System.out.println("JSONArray scaricato: "+ja);
-				System.out.println("IL JSONArray scaricato ha "+ ja.size()+" elementi:");
-			
-				for(int i=0;i<ja.size();i++) {
-					jo = (JSONObject) ja.get(i);
-					System.out.println(i+") "+jo.get("name"));
-				}
-			}*/
 
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return jo;
-	}
-	
-	
-	public void forecastNome(String nomeCitta) throws MalformedURLException
-	{
-		call.setNomeCitta(nomeCitta);
-		//System.out.println(call.getNomeCitta());
-		
-		String indirizzo = "http://api.openweathermap.org/data/2.5/forecast?q="+ call.getNomeCitta() +"&appid="+ call.getKey() +call.getUnita();
-		//System.out.println(indirizzo);
-		
-		URL url = new URL (indirizzo);
-		
-		try {
-			URLConnection openConnection = new URL(indirizzo).openConnection();
-			InputStream in = openConnection.getInputStream();
-			
-			String data = "";
-			String line = "";
-			try {
-			   InputStreamReader inR = new InputStreamReader( in );
-			   BufferedReader buf = new BufferedReader( inR );
-			  
-			   while ( ( line = buf.readLine() ) != null ) {
-				   data+= line;
-			   }
-			} 
-			finally 
-			{
-			   in.close();
-			}
-			
-				jo = (JSONObject) JSONValue.parseWithException(data);	 //parse JSON Object
-				//System.out.println("JSONObject scaricato: "+ jo);
-			/*} else {
-				ja = (JSONArray) JSONValue.parseWithException(data);	//parse JSON Array
-				System.out.println("JSONArray scaricato: "+ja);
-				System.out.println("IL JSONArray scaricato ha "+ ja.size()+" elementi:");
-			
-				for(int i=0;i<ja.size();i++) {
-					jo = (JSONObject) ja.get(i);
-					System.out.println(i+") "+jo.get("name"));
-				}
-			}*/
-
-		} catch (IOException | ParseException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		call.setCast(jo);
-		System.out.println("FATTO CHIAMATA NOME");
-		//return jo;
-		
-	}
-	
-	
-	
+	/**
+	 * Questo metodo effettua la chiamata all'API OpenWeatherMap.
+	 * Chiama il metodo nameToID() per convertire nomeCitta in un ID da usare poi per fare la chiamata
+	 * 
+	 * @param nomeCitta Questo parametro contiene il nome inserito dall'utente in fase di chiamata.
+	 * @return Il metodo restituisce un oggetto di tipo JSONObject contenete il json col risultato della chiaamata.
+	 * @throws MalformedURLException
+	 */
 	public JSONObject forecastID(String nomeCitta) throws MalformedURLException
 	{
-		
 		nameToID(nomeCitta);
 		
 		String indirizzo = "http://api.openweathermap.org/data/2.5/forecast?id="+ call.getId() +"&appid="+ call.getKey() +call.getUnita();
 		System.out.println(indirizzo);
-		
 		
 		URL url = new URL (indirizzo);
 		
@@ -184,6 +68,89 @@ public class Services
 		
 		return jo;
 	}
+	
+	/**
+	 * Metodo che converte il nome della città inserita dall'utente in un ID usato poi per la chiamata.
+	 * Per fare questa conversione, chiama il metodo forecastNome() passandogli il nome della città.
+	 * A questo punto, accede al JSONObject cast, e salva una stringa contente l'ID sul rispettivo campo dell'oggetto call.
+	 * 
+	 * 
+	 * @param nome Questo parametro corrisponde al nome della città inserito dall'utente
+	 * @throws MalformedURLException
+	 */
+	public void nameToID(String nome) throws MalformedURLException
+	{
+		String ID ="";
+		
+		forecastNome(nome);
+		System.out.println("CHIAMATO FORCASTNAME DA nameToID");
+		JSONObject app = call.getCast();
+		JSONObject city = (JSONObject) app.get("city");
+		Object idCitta =city.get("id");
+		System.out.println("idCitta: "+idCitta);
+		
+		ID =idCitta.toString();
+		System.out.println("id:" +ID);
+		call.setId(ID);
+		
+		System.out.println("CONVERTITO ID");
+		
+	}
+	
+	/**
+	 * Questo metodo effettua una chiamata all'API OpenWeatherMap.
+	 * A differenza del metodo forecastID(), usa il nome della città. 
+	 * Una volta fatta la chiamata e ottenuto il JSON, lo salva nel campo cast dell'oggetto call.
+	 * 
+	 * @param nomeCitta QUesto parametro contiene il nome della città inserito dall'utente.
+	 * @throws MalformedURLException
+	 */
+	public void forecastNome(String nomeCitta) throws MalformedURLException
+	{
+		call.setNomeCitta(nomeCitta);
+		//System.out.println(call.getNomeCitta());
+		
+		String indirizzo = "http://api.openweathermap.org/data/2.5/forecast?q="+ call.getNomeCitta() +"&appid="+ call.getKey() +call.getUnita();
+		//System.out.println(indirizzo);
+		
+		URL url = new URL (indirizzo);
+		
+		try {
+			URLConnection openConnection = new URL(indirizzo).openConnection();
+			InputStream in = openConnection.getInputStream();
+			
+			String data = "";
+			String line = "";
+			try {
+			   InputStreamReader inR = new InputStreamReader( in );
+			   BufferedReader buf = new BufferedReader( inR );
+			  
+			   while ( ( line = buf.readLine() ) != null ) {
+				   data+= line;
+			   }
+			} 
+			finally 
+			{
+			   in.close();
+			}
+			
+				jo = (JSONObject) JSONValue.parseWithException(data);
+
+		} catch (IOException | ParseException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		call.setCast(jo);
+		System.out.println("FATTO CHIAMATA NOME");
+		//return jo;
+		
+	}
+	
+	
+	
+	
 }
 
 	//per il prossimo futuro
