bff9f60444d9f3edebaea9c7601a2384cb1591b9
«Marco Olivi» <S1092635@studenti.univpm.it>
2021-01-06 12:15:39 +0100

    documentazione

--- MeteoRite/src/main/java/ProgettoEsame/MeteoRite/Model/APICall.java
+++ MeteoRite/src/main/java/ProgettoEsame/MeteoRite/Model/APICall.java
@@ -1,9 +1,11 @@
+
 package ProgettoEsame.MeteoRite.Model;
 
 import org.json.simple.JSONObject;
 
 public class APICall 
 {
+	
 	private String nomeCitta;
 	private String unita = "&units=metric";
 	private static String key = "3b5be1096db5f3df2026d19e0acef27d";
@@ -12,17 +14,36 @@ public class APICall
 	
 	//private String indirizzo = "http://api.openweathermap.org/data/2.5/forecast?q=="+ nomeCitta +"&appid="+ key + unita;
 	
+	/**
+	 * questo è un overload del costruttore 
+	 * serve per la chiamata della città 
+	 * @param nomeCitta
+	 */
 	public APICall(String nomeCitta)
 	{
 		this.nomeCitta=nomeCitta;
 	}
 	
+	/**
+	 * questo è un overload del costruttore 
+	 * @param nomeCitta
+	 * @param id
+	 */
 	public APICall(String nomeCitta, String id)
 	{
 		this.nomeCitta=nomeCitta;
 		this.id=id;
 	}
-	
+/**
+ * questo è un overload del costruttore
+ * serve per la chiamata all'API per nome di città e per ID corrispondente alla città.
+ * Abbiamo poi specificato nella chiamata 
+ * l'unità di misura della temperatura in celsius e non in 
+ * kelvin che invece era di default 
+ * @param nomeCitta
+ * @param id
+ * @param unita
+ */
 	public APICall(String nomeCitta,String id, String unita)
 	{
 		this.nomeCitta=nomeCitta;
@@ -38,11 +59,20 @@ public class APICall
 		this.indirizzo = indirizzo;
 	}
 	*/
-
+	
+	
+/**
+ * prende la key per accedere all'API
+ * @return
+ */
 	public static String getKey() {
 		return key;
 	}
 	
+	/**
+	 * sono un get  e un set riferito all'unità di musira 
+	 * @return
+	 */
 	public String getUnita() {
 		return unita;
 	}
@@ -50,6 +80,10 @@ public class APICall
 		this.unita = unita;
 	}
 	
+	/**
+	 * sono un get  e un set riferito al nome della città per la  chiamata all' API
+	 * @return
+	 */
 	public String getNomeCitta() {
 		return nomeCitta;
 	}
@@ -58,6 +92,10 @@ public class APICall
 		this.nomeCitta = nomeCitta;
 	}
 
+	/**
+	 * sono un get  e un set riferito al ID della città per la chiamata all' API
+	 * @return
+	 */
 	public String getId() {
 		return id;
 	}
@@ -66,6 +104,10 @@ public class APICall
 		this.id = id;
 	}
 
+	/**
+	 * sono un get  e un set riferito JASON
+	 * @return
+	 */
 	public JSONObject getCast() {
 		return cast;
 	}
