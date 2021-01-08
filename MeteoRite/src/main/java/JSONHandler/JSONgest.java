e39b9be06c125a639ee2d78776b02e625893672c
Davide <Davide@CompiutoMK3.lan>
2021-01-08 15:47:59 +0100

    Sistemato lettura JSONArray. adesso fa la media di tutto.

--- MeteoRite/src/main/java/JSONHandler/JSONgest.java
+++ MeteoRite/src/main/java/JSONHandler/JSONgest.java
@@ -8,9 +8,9 @@ public class JSONgest
 {
 	
 	
-	public double media(JSONArray ja)
+	public double mediaTemp(JSONArray ja)
 	{
-			double media = 0;
+			double media = 0.0;
 			JSONObject jo = new JSONObject();
 			
 			int i= 0;
@@ -24,10 +24,40 @@ public class JSONgest
 
 			return media;
 	}
-		
-	public void prova()
-		{
-			System.out.println("FUNZIONA");
-		}
+	
+	public double mediaMin(JSONArray ja)
+	{
+			double media = 0.0;
+			JSONObject jo = new JSONObject();
+			
+			int i= 0;
+			for( i=0 ; i<ja.size();i++)
+			{
+				jo = (JSONObject) ja.get(i);
+				media += (int)jo.get("temp_min");
+				
+			}
+			media = (media/i);
 
+			return media;
+	}
+	
+	public double mediaMax(JSONArray ja)
+	{
+			double media = 0.0;
+			JSONObject jo = new JSONObject();
+			
+			int i= 0;
+			for( i=0 ; i<ja.size();i++)
+			{
+				jo = (JSONObject) ja.get(i);
+				media += (int)jo.get("temp_max");
+				
+			}
+			media = (media/i);
+
+			return media;
+	}
+		
+	
 }
