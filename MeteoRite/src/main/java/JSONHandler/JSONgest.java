c851943b6edb707b3f4aab1a3ad94c3cb341200d
Davide <Davide@CompiutoMK3.lan>
2021-01-09 11:24:49 +0100

    mediaMin/Max adesso sono maxmax e minmin; trovano massimo e minimo

--- MeteoRite/src/main/java/JSONHandler/JSONgest.java
+++ MeteoRite/src/main/java/JSONHandler/JSONgest.java
@@ -12,6 +12,7 @@ public class JSONgest
 	
 	public double mediaTemp(JSONArray ja)
 	{
+		System.out.println("MEDIA TEMP");
 			double media = 0.0;
 			JSONObject jo = new JSONObject();
 			
@@ -27,42 +28,47 @@ public class JSONgest
 			return media;
 	}
 	
-	public double mediaMin(JSONArray ja)
+	public double minMin(JSONArray ja)
 	{
-			double media = 0.0;
+		System.out.println("MIN MIN");
+			double m = 0.0;
 			JSONObject jo = new JSONObject();
 			
 			int i= 0;
 			for( i=0 ; i<ja.size();i++)
 			{
 				jo = (JSONObject) ja.get(i);
-				media += (int)jo.get("temp_min");
-				
+				if((int)(jo.get("temp_min")) < m)
+				{
+					m += (int)jo.get("temp_min");
+				}
 			}
-			media = (media/i);
 
-			return media;
+			return m;
 	}
 	
-	public double mediaMax(JSONArray ja)
+	public double maxMax(JSONArray ja)
 	{
-			double media = 0.0;
+		System.out.println("MAX MAX");
+			double m = 0.0;
 			JSONObject jo = new JSONObject();
 			
 			int i= 0;
 			for( i=0 ; i<ja.size();i++)
 			{
 				jo = (JSONObject) ja.get(i);
-				media += (int)jo.get("temp_max");
-				
+				if((int)(jo.get("temp_max")) > m)
+				{
+					m= (int)jo.get("temp_max");
+				}
 			}
-			media = (media/i);
 
-			return media;
+			return m;
 	}
 		
 	public JSONObject mediaBox(JSONArray arr)
 	{
+		System.out.println("MEDIA BOX");
 		//JSONArray fin = new JSONArray();
 		JSONObject box = new JSONObject();
 		
@@ -70,8 +76,8 @@ public class JSONgest
 		
 		
 			double t = mediaTemp(arr);
-			double min = mediaMin(arr);
-			double max = mediaMax(arr);
+			double min = minMin(arr);
+			double max = maxMax(arr);
 			//j++;
 			
 			box = serv.boxer(t, min, max);
@@ -81,6 +87,7 @@ public class JSONgest
 	
 	public JSONArray arrayLoader(JSONArray jarr)
 	{
+		System.out.println("ARRAY LOADER");
 		
 		JSONObject ogg = null;
 		JSONObject box = null;
@@ -107,8 +114,12 @@ public class JSONgest
 	}
 	
 	
-	public JSONArray dataFilter(JSONArray ja, JSONArray arr[])
+	public void dataFilter(JSONArray ja, JSONArray arr[])
 	{
+		System.out.println("DATA FILTER in teoria era questo che dava problemi");
+		//se è effettivamente qui il problema, passare come parametri 2 array (ja e arr[i] facendo un for dall'altra parte
+		//cambiare quindi il for in modo che mette gli elementi solo se data corrisponde
+		
 		JSONObject app = (JSONObject) ja.get(0);
 		String d = (String) app.get("data");
 		//JSONArray arr[] = new JSONArray[5];
@@ -116,25 +127,34 @@ public class JSONgest
 		int j = 0;	
 		for(int i = 0; i<ja.size();i++)
 		{
+			if(j<5)
+			{
 			app = (JSONObject) ja.get(i);
 			String d2 = (String) app.get("data");
 			
 			if(d2.equals(d))
 			{
 				arr[j].add(app);
+				System.out.println("D2 è uguale. j vale:"+j +"ci ho messo" +app);
 			}
 			else
 			{
 				j++;
 				arr[j].add(app);
+				System.out.println("D2 non è uguale. incremento j adesso vale: "+j +"ci ho messo:"+app);
 			}
+			
+			}
+			else
+				break;
 		}
 		
-		return ja;
+		//return arr;
 	}
 	
 	public JSONArray finalArrayLoader(JSONArray arr[])
 	{
+		System.out.println("FINAL ARRAY LOADER");
 		JSONArray fin = new JSONArray();
 		JSONObject box = new JSONObject();
 		
